35f575fd0e0641315ed9cdd42485b65a8f15e6e2
netLee <wltjr1426@naver.com>
2021-02-01 15:08:07 +0900

    Setting

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from "fireabse/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCVtUxgXzmpFvy6G34Ll91A9IFcdHi6irs",
+  authDomain: "jswitter-41de0.firebaseapp.com",
+  projectId: "jswitter-41de0",
+  storageBucket: "jswitter-41de0.appspot.com",
+  messagingSenderId: "256757313787",
+  appId: "1:256757313787:web:6a97f7544ce721b3dcc05e",
+};
+
+firebase.initializeApp(firebaseConfig);
