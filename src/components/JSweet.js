e63e8ab6c96905c76faf437ff024d78ad7c7805f
netLee <wltjr1426@naver.com>
2021-02-09 11:54:31 +0900

    Update Edit

--- src/components/JSweet.js
+++ src/components/JSweet.js
@@ -1,23 +1,53 @@
 import { dbService } from "fbase";
-import React from "react";
+import React, { useState } from "react";
 
 // isOwner => 내가 주인이면 이 버튼 fragment 들을 볼 수 있음
 const JSweet = ({ jsweetObj, isOwner }) => {
+  const [editing, setEditing] = useState(false);
+  const [newJSweet, setNewJSweet] = useState(jsweetObj.text);
   const onDeleteClick = async () => {
     const ok = window.confirm("Are you sure you want to delete this jsweet?");
     console.log(ok);
     if (ok) {
       // delete jsweet
+      // doc 경로는 firebase보면 됨
       await dbService.doc(`jsweets/${jsweetObj.id}`).delete();
     }
+  };
+  const toggleEditing = () => setEditing((prev) => !prev);
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewJSweet(value);
   };
   return (
     <div>
       <h4>{jsweetObj.text}</h4>
-      {isOwner && (
+      {editing ? (
         <>
-          <button onClick={onDeleteClick}>Delete JSweet</button>
-          <button>Edit JSweet</button>
+          <form onSubmit={onSubmit}>
+            <input
+              type="text"
+              placeholder="Edit your jsweet"
+              value={newJSweet}
+              required
+              onChange={onChange}
+            />
+          </form>
+          <button onClick={toggleEditing}>Cancle</button>
+        </>
+      ) : (
+        <>
+          {isOwner && (
+            <>
+              <button onClick={onDeleteClick}>Delete JSweet</button>
+              <button onClick={toggleEditing}>Edit JSweet</button>
+            </>
+          )}
         </>
       )}
     </div>
